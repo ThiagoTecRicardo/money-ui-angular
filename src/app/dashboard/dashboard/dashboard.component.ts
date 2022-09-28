eaf525f164fcd414b610dd5a70f665d4ee9d73e7
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2022-09-28 15:01:06 -0300

    uscando dados do gráfico de linhas

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -11,20 +11,7 @@ export class DashboardComponent implements OnInit {
   
 
   pieChartData: any;
-  lineChartData = {
-    labels: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
-    datasets: [
-      {
-        label: 'Receitas',
-        data: [4, 10, 18, 5, 1, 20, 3],
-        borderColor: '#109618'
-      }, {
-        label: 'Despesas',
-        data: [10, 15, 8, 5, 1, 7, 9],
-        borderColor: '#D62B00'
-      }
-    ]
-  };
+  lineChartData: any;
 
   basicData = {
     labels: ['Janeiro', 'Fevereiro', 'Março', 'April', 'Maio', 'Junho', 'Julho', 'Agosto'],
@@ -49,6 +36,7 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
 
     this.configurarGraficoPizza();
+    this.configurarGraficoLinha();
   }
 
   configurarGraficoPizza(){
@@ -60,11 +48,70 @@ export class DashboardComponent implements OnInit {
           {
             data: dados.map(dado => dado.total),
             backgroundColor: ['#FF9900', '#109618', '#990099', '#3B3EAC', '#0099C6',
-                '#DD4477', '#3366CC', '#DC3912']
+                '#DD4477', '#3366CC', '#DC3912', '##ADD8E6', '	#2F4F4F', '	#FFFF00', '#FFC0CB', '#4B0082']
           }
         ]
       };
     });
+  }
+  configurarGraficoLinha() {
+    this.dashboardService.lancamentosPorDia()
+      .then(dados => {
+        const diasDoMes = this.configurarDiasMes();
+
+        const totaisReceitas = this.totaisPorCadaDiaMes(
+          dados.filter(dado => dado.tipo === 'RECEITA'), diasDoMes);
+
+        const totaisDespesas = this.totaisPorCadaDiaMes(
+          dados.filter(dado => dado.tipo === 'DESPESA'), diasDoMes);
+
+        this.lineChartData = {
+          labels: diasDoMes,
+          datasets: [
+            {
+              label: 'Receitas',
+              data: totaisReceitas,
+              borderColor: '#109618'
+            }, {
+              label: 'Despesas',
+              data: totaisDespesas,
+              borderColor: '#D62B00'
+            }
+          ]
+        }
+      });
+  }
+
+  private totaisPorCadaDiaMes(dados: any, diasDoMes: any) {
+    const totais: number[] = [];
+    for (const dia of diasDoMes) {
+      let total = 0;
+
+      for (const dado of dados) {
+        if (dado.dia.getDate() === dia) {
+          total = dado.total;
+          break;
+        }
+      }
+
+      totais.push(total);
+    }
+
+    return totais;
+  }
+
+  private configurarDiasMes() {
+    const mesReferencia = new Date();
+    mesReferencia.setMonth(mesReferencia.getMonth() + 1);
+    mesReferencia.setDate(0);
+    const quantidade = mesReferencia.getDate();
+
+    const dias: number[] = [];
+    for (let i = 1; i <= quantidade; i++) {
+      dias.push(i);
+    }
+
+    return dias;
   }
 }
 
