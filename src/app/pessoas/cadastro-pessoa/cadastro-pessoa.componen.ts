fe73f479f7dfdaa4ae1fe41318101c41b228e3f5
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2022-10-04 01:26:00 -0300

    incluindo novo contato

--- src/app/pessoas/cadastro-pessoa/cadastro-pessoa.componen.ts
+++ src/app/pessoas/cadastro-pessoa/cadastro-pessoa.componen.ts
@@ -42,6 +42,16 @@ export class CadastroPessoalComponent implements OnInit {
     this.contato = new Contato();
   }
 
+  confirmarContato(frm: NgForm) {
+    this.pessoa.contatos.push(this.clonarContato(this.contato!));
+    this.exbindoFormularioContato = false;
+    frm.reset();
+  }
+
+  clonarContato(contato: Contato): Contato {
+    return new Contato(contato.codigo, contato.nome, contato.email, contato.telefone);
+  }
+
   get editando() {
     return Boolean(this.pessoa.codigo)
   }
