fd9559712440684190ca413896a3638d8fdc2c89
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2022-10-05 09:46:45 -0300

    update categoria service

--- src/app/categorias/categoria.service.ts
+++ src/app/categorias/categoria.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { environment } from 'src/environments/environment';
+import { Categoria } from '../core/model';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +19,6 @@ export class CategoriaService {
 
       return this.http.get(this.categoriasUrl)
       .toPromise();
-
-
   }
+
 }
